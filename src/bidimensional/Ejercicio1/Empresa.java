3a42738d1383aeff398670a08832c26940e71dfb
marcos <twden@DESKTOP-1BDR09C>
2025-02-20 20:11:35 +0100

    Version 1.2

--- src/bidimensional/Ejercicio1/Empresa.java
+++ src/bidimensional/Ejercicio1/Empresa.java
@@ -4,17 +4,28 @@
  */
 package bidimensional.Ejercicio1;
 
+import Fecha.Fecha;
+
 /**
  *
  * @author twden
  */
 public class Empresa {
+
     static Empleado empleados[];
-    
-    public void inicializarEmpleados(){
+
+    public void inicializarEmpleados() {
         int nEmpleados = Numeros.UtilidadesNumeros.pedirNumeroEntero("Introduzca el numero de empleados que tiene la empresa", 0);
-        empleados = new Empleado [nEmpleados];
+        empleados = new Empleado[nEmpleados];
     }
-    
-    
+
+    public void cargarDatos() {
+        for (int pos = 0; pos < empleados.length; pos++) {
+            String nombre = Stringc.UtilidadesString.pedirString("Introduzca el nombre del empleado:  " + pos);
+            Fecha fAlta = Stringc.UtilidadesString.pedirFecha("Introduzca la fecha de alta de:  " + nombre);
+            int numeroHijos = Numeros.UtilidadesNumeros.pedirNumeroEntero("Introduzca el numeor de hijos del empleado:  " + nombre, 0);
+            
+        }
+    }
+
 }
